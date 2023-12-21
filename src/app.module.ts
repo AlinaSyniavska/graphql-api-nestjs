38cb41e150c834963989389340ac36e84c0656b5
AlinaSyniavska <alina22syniavska@gmail.com>
2023-12-21 15:37:52 +0200

    Seeded the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { MovieModule } from './movie/movie.module';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    MovieModule,
   ],
   controllers: [],
   providers: [],
