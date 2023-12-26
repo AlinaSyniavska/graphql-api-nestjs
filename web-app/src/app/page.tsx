a207e7abaf67e9f9e02012036df28bb1864f4ed7
AlinaSyniavska <alina22syniavska@gmail.com>
2023-12-26 17:03:07 +0200

    Changed the project structure. Separated backend and frontend.

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,4 +1,15 @@
+import { gql } from '@apollo/client';
+
 export default function Home() {
+
+  const exampleQuery = gql`
+  query AnExample($id: Int!) {
+    example(id: $id) {
+      name
+    }
+  }
+`
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
