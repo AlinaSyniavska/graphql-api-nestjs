e9ea6bd9dd248452cbc44b681b74dca2a7789af6
AlinaSyniavska <alina22syniavska@gmail.com>
2023-12-28 16:33:58 +0200

    Implemented Apollo Client in Next.js with TypeScript & GraphQL Code Generator

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,29 +1,35 @@
-'use client'
+'use client';
 
-import { gql, useQuery } from '@apollo/client';
-import { MoviesQuery } from '@/gql/graphql';
+import { useQuery } from '@apollo/client';
+import { getDataFromTree } from '@apollo/client/react/ssr';
 
-export default function Home() {
+import { CHARACTERS_QUERY } from '../../graphql/queries';
+import withApollo from '../../lib/withApollo';
 
-  const allMoviesQuery = gql`
-  query Movies {
-    getAllMovies {
-      title
-      description
-   }
-  }
-  `;
-
-  const { data, error } = useQuery<MoviesQuery>(allMoviesQuery);
-
-  console.log('1 **********');
-  console.log(data);
-  console.log('2 **********');
-  console.log(error);
+function Home() {
+  const { data, error, loading } = useQuery(CHARACTERS_QUERY);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        {JSON.stringify(data)}
+
+
+{/*        {characters.map((character) => (
+          <div key={character.id}>
+            <Image
+              src={character.image}
+              alt={character.name}
+              width="200px"
+              height="200px"
+            />
+            <Link href="/characters/[id]" as={`/characters/${character.id}`}>
+              {character.name}
+            </Link>
+          </div>
+        ))}*/}
+
+      </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
@@ -47,3 +53,5 @@ export default function Home() {
     </main>
   );
 };
+
+export default withApollo(Home, { getDataFromTree });
